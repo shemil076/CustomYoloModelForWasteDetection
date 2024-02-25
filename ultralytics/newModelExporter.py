c4b51e2bc352adc74dcfb365261334ee24972f54
shemil076 <shemil076@gmail.com>
2024-02-25 19:50:26 +0530

    Added a CBAM layer to the head of yolov8

--- ultralytics/newModelExporter.py
+++ ultralytics/newModelExporter.py
@@ -18,23 +18,3 @@ results = model.val()
 
 
 
-# import torch
-# from ultralytics import YOLO
-
-# # 1. Configuration Setup
-# config = {
-#     'data': '/Users/pramudithakarunarathna/Documents/IIT BEng Software Engineering/FYP/Implementations/ultralytics-main/ultralytics/cfg/datasets/datasetcust.yaml',
-#     'model': '/Users/pramudithakarunarathna/Documents/IIT BEng Software Engineering/FYP/Implementations/ultralytics-main/ultralytics/cfg/models/v8/yolov8n.yaml',
-#     'epochs': 20,  # Adjust as needed
-#     'loss': {
-#         'name': 'EIoULoss',
-#         'reduction': 'mean'
-#     }
-# }
-
-# # 2. Model and Datasets
-# model = YOLO(config['model']) 
-# results = model.train(data=config['data'], epochs=config['epochs'])
-
-# ... (Rest of your evaluation, export code can follow as needed)
-
