67a3fc9cf88634410a3b5d5242a8b74e82c28c9b
shemil076 <shemil076@gmail.com>
2024-03-20 15:11:28 +0530

    updated the batch size

--- ultralytics/newModelExporter.py
+++ ultralytics/newModelExporter.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 # Create a new YOLO model from scratch
 model = YOLO('/kaggle/working/CustomYoloModelForWasteDetection/ultralytics/cfg/models/v8/yolov8l.yaml')
 
-results = model.train(data='/kaggle/working/CustomYoloModelForWasteDetection/ultralytics/cfg/datasets/datasetcust.yaml', epochs=100, imgsz=640, batch=32, lr0=0.005, lrf=0.65)
+results = model.train(data='/kaggle/working/CustomYoloModelForWasteDetection/ultralytics/cfg/datasets/datasetcust.yaml', epochs=100, imgsz=640, batch=16, lr0=0.005, lrf=0.65)
 
 # Evaluate the model's performance on the validation set
 results = model.val()
