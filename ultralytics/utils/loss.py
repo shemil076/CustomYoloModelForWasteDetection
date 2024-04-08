4686a54dfba7234f630317060d8307d7a10bdb69
shemil076 <shemil076@gmail.com>
2024-04-08 17:23:15 +0530

    implemented use bbox_iou_for_nms

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -5,6 +5,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from .ops import bbox_iou_for_nms
+
 from ultralytics.utils.metrics import OKS_SIGMA
 from ultralytics.utils.ops import crop_mask, xywh2xyxy, xyxy2xywh
 from ultralytics.utils.tal import RotatedTaskAlignedAssigner, TaskAlignedAssigner, dist2bbox, dist2rbox, make_anchors
@@ -225,6 +227,48 @@ def bbox_eiou(pred_bboxes, target_bboxes, xywh=True):
 #             + F.cross_entropy(pred_dist, tr.view(-1), reduction="none").view(tl.shape) * wr
 #         ).mean(-1, keepdim=True)
 
+# class BboxLoss(nn.Module):
+#     """Criterion class for computing training losses during training."""
+
+#     def __init__(self, reg_max, use_dfl=False):
+#         """Initialize the BboxLoss module with regularization maximum and DFL settings."""
+#         super().__init__()
+#         self.reg_max = reg_max
+#         self.use_dfl = use_dfl
+
+#     def forward(self, pred_dist, pred_bboxes, anchor_points, target_bboxes, target_scores, target_scores_sum, fg_mask):
+#         """IoU loss."""
+#         weight = target_scores.sum(-1)[fg_mask].unsqueeze(-1)
+#         iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, EIoU=True,Focal=True)
+#         loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
+
+#         # DFL loss
+#         if self.use_dfl:
+#             target_ltrb = bbox2dist(anchor_points, target_bboxes, self.reg_max)
+#             loss_dfl = self._df_loss(pred_dist[fg_mask].view(-1, self.reg_max + 1), target_ltrb[fg_mask]) * weight
+#             loss_dfl = loss_dfl.sum() / target_scores_sum
+#         else:
+#             loss_dfl = torch.tensor(0.0).to(pred_dist.device)
+
+#         return loss_iou, loss_dfl
+
+#     @staticmethod
+#     def _df_loss(pred_dist, target):
+#         """
+#         Return sum of left and right DFL losses.
+
+#         Distribution Focal Loss (DFL) proposed in Generalized Focal Loss
+#         https://ieeexplore.ieee.org/document/9792391
+#         """
+#         tl = target.long()  # target left
+#         tr = tl + 1  # target right
+#         wl = tr - target  # weight left
+#         wr = 1 - wl  # weight right
+#         return (
+#             F.cross_entropy(pred_dist, tl.view(-1), reduction="none").view(tl.shape) * wl
+#             + F.cross_entropy(pred_dist, tr.view(-1), reduction="none").view(tl.shape) * wr
+#         ).mean(-1, keepdim=True)
+
 class BboxLoss(nn.Module):
     """Criterion class for computing training losses during training."""
 
@@ -237,8 +281,16 @@ class BboxLoss(nn.Module):
     def forward(self, pred_dist, pred_bboxes, anchor_points, target_bboxes, target_scores, target_scores_sum, fg_mask):
         """IoU loss."""
         weight = target_scores.sum(-1)[fg_mask].unsqueeze(-1)
-        iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True)
-        loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
+        # iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, EIoU=True,Focal=True)
+        iou = bbox_iou_for_nms(pred_bboxes[fg_mask], target_bboxes[fg_mask])
+        
+        if type(iou) is tuple:
+            if len(iou) == 2:
+                loss_iou = ((1.0 - iou[0]) * iou[1].detach() * weight).sum() / target_scores_sum
+            else:
+                loss_iou = (iou[0] * iou[1] * weight).sum() / target_scores_sum
+        else:
+            loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
 
         # DFL loss
         if self.use_dfl:
@@ -252,20 +304,14 @@ class BboxLoss(nn.Module):
 
     @staticmethod
     def _df_loss(pred_dist, target):
-        """
-        Return sum of left and right DFL losses.
-
-        Distribution Focal Loss (DFL) proposed in Generalized Focal Loss
-        https://ieeexplore.ieee.org/document/9792391
-        """
+        """Return sum of left and right DFL losses."""
+        # Distribution Focal Loss (DFL) proposed in Generalized Focal Loss https://ieeexplore.ieee.org/document/9792391
         tl = target.long()  # target left
         tr = tl + 1  # target right
         wl = tr - target  # weight left
         wr = 1 - wl  # weight right
-        return (
-            F.cross_entropy(pred_dist, tl.view(-1), reduction="none").view(tl.shape) * wl
-            + F.cross_entropy(pred_dist, tr.view(-1), reduction="none").view(tl.shape) * wr
-        ).mean(-1, keepdim=True)
+        return (F.cross_entropy(pred_dist, tl.view(-1), reduction='none').view(tl.shape) * wl +
+             F.cross_entropy(pred_dist, tr.view(-1), reduction='none').view(tl.shape) * wr).mean(-1, keepdim=True)
 class RotatedBboxLoss(BboxLoss):
     """Criterion class for computing training losses during training."""
 
