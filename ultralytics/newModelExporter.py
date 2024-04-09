b4f145b3fe0d0d6ba68686ff12d267b168b9802f
shemil076 <shemil076@gmail.com>
2024-04-09 17:04:07 +0530

    trying soft-nms with iou

--- ultralytics/newModelExporter.py
+++ ultralytics/newModelExporter.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 # Create a new YOLO model from scratch
 model = YOLO('/content/CustomYoloModelForWasteDetection/ultralytics/cfg/models/v8/yolov8m.yaml')
 
-results = model.train(data='/content/ModelTrainingTestData/data.yaml', epochs=50, imgsz=640, batch=16, optimizer='SGD', patience=10,device=0)
+results = model.train(data='/content/ModelTrainingTestData/data.yaml', epochs=10, imgsz=640, batch=16, optimizer='SGD', patience=10,device=0)
 
 # Evaluate the model's performance on the validation set
 results = model.val()
